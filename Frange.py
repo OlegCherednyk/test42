95a738364ac799d7b0dc9fb012bd6c2b4081777a
Oleg <oleg2759064@gmail.com>
2020-07-14 14:11:27 +0300

    Fix3 Frange

--- Frange.py
+++ Frange.py
@@ -20,10 +20,10 @@ class Frange:
 
             return result
 
-    def __init__(self, lim2, lim1="default", step=1):
+    def __init__(self, lim2, lim1=None, step=1):
         self._step = step
 
-        if lim1 == "default":
+        if lim1 is None:
             self._lim1 = 0
             self._lim2 = lim2
         else:
